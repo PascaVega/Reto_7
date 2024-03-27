fef9910ee32efbda3912b9ec8e1b205fbcd5335f
PascaVega <159050569+PascaVega@users.noreply.github.com>
2024-03-27 18:39:57 -0500

    #RETO 7 - Punto 2.py

--- #RETO 7 - Punto 2.py
+++ #RETO 7 - Punto 2.py
@@ -1,17 +1,18 @@
 #RETO 7 - Punto 2
 #Imprimir un listado con los números impares desde 1 hasta 999 y seguidamente otro listado con los números pares desde 2 hasta 1000.
 
-n : int = 1
-m : int = 2
+n : int = 1 #Variable para los número impares
+m : int = 2 #Variable para los número pares
 
 if __name__ == "__main__":
+    #Imprimir los números impares
     print("Números impares del 1 al 999")
     while n <= 999:
         print(str(n))
         n += 2
-
+        
+    #Imprimir los números pares
     print("Números pares del 2 al 1000")
-
     while m <= 1000:
         print(str(m))
         m += 2
