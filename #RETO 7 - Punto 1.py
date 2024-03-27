652da515a5c25feda5dac99b80b04ffdd9cce886
PascaVega <159050569+PascaVega@users.noreply.github.com>
2024-03-27 18:38:10 -0500

    #RETO 7 - Punto 1.py

--- #RETO 7 - Punto 1.py
+++ #RETO 7 - Punto 1.py
@@ -3,9 +3,11 @@
 
 n : int = 1
 
+#Iniciar el código
 if __name__ == "__main__":
     print("Números del 1 al 100 con su respectivo cuadrado.")
 
+    #Ciclo while para imprimir el listado
     while n <= 100:
         m : int = n**2
         print(str(n) , str(m))
