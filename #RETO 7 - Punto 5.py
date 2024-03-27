bd78144a180516c9a6e118e5129da52f5c556236
PascaVega <159050569+PascaVega@users.noreply.github.com>
2024-03-27 18:58:19 -0500

    #RETO 7 - Punto 5.py

--- #RETO 7 - Punto 5.py
+++ #RETO 7 - Punto 5.py
@@ -2,15 +2,19 @@
 #Imprimir el factorial de un número natural n dado.
 
 def introducir():
+    #Se introduce el valor de a variable
     numero : int = int(input("Introduzca un numero natural para imprimir el factorial. Ejemplo: 5: "))
     desarrollo(numero)
 
 def desarrollo(numero):
+    #Definimos dos nuevas variables
     n : int = numero
     factorial : int = 1
+    #Con laposibilidad de que el número sea 0 se define instantaneamente el valor del factorial
     if n == 0:
         print("El factorial de 0 es 1")
         return
+    #El ciclo while continuará siempre y cuando n sea mayor a 1
     while n>1:
         factorial *= n
         n -=1
@@ -26,6 +30,7 @@ if __name__ == "__main__":
     while True:
         introducir()
         opcion = continuar()
+        #Saber si el usuario quiere volver a ejecutar el código
         if opcion == 2:
             break
         elif opcion != 1 and 2:
