87eda9f1e6b2f345ed0ff5f3f7fc8278eecb3f63
PascaVega <159050569+PascaVega@users.noreply.github.com>
2024-03-27 19:12:07 -0500

    #RETO 7 - Punto 7.py

--- #RETO 7 - Punto 7.py
+++ #RETO 7 - Punto 7.py
@@ -2,21 +2,26 @@
 #Implementar un programa que ingrese un número de 2 a 50 y muestre sus divisores.
 
 def introducir():
+    #Se introduce el número
     numero : int = int(input("Introduzca un numero entre 2 y 50 para conocer sus divisores. Ejemplo: 5: "))
     desarrollo(numero)
 
 def desarrollo(numero):
+    #Se analiza si el número no está dentro del intervalo
     if numero <2 or numero > 50:
         print(f"{numero} no está en el rango")
         continuar()
     else:
         print(f"Los divisores de {numero} son:")
+        #Se define una nueva variable
         n : int = numero
         while n>0:
+            #Si el número es divisible por n entonces que lo imprima y luego le reste 1 a n
             if numero%n == 0:
                 print(str(n))
                 n -= 1
                 continue
+            #Si no es divisible entonces que solo le reste 1
             else:
                 n -=1
                 continue
@@ -32,6 +37,7 @@ if __name__ == "__main__":
     while True:
         introducir()
         opcion = continuar()
+        #El usuario decide si quiere volver a ejecutar el código
         if opcion == 2:
             break
         elif opcion != 1 and 2:
